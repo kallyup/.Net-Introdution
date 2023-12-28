07414c93338c5d91e6ac7bd665bbf4d5126972fa
Débora Macêdo <deboramacedo886@gmail.com>
2023-12-28 17:34:15 -0300

    classe e datetime

--- projeto2/Program.cs
+++ projeto2/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("hy");
+﻿using projeto2.Models;
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "deb";
+pessoa1.Idade = 25;
+pessoa1.Apresentar();
+
+DateTime dataActual = DateTime.Now;
+Console.WriteLine(dataActual);
+Console.WriteLine(dataActual.ToString("dd/MM/yyyy"));
