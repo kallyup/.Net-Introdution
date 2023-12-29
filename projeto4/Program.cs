a518065e7baf7a7e178e8778bfa2f00d7a29429d
Débora Macêdo <deboramacedo886@gmail.com>
2023-12-29 15:23:32 -0300

    laço de repetição , calculadora

---
+++ projeto4/Program.cs
@@ -0,0 +1,13 @@
+﻿using projeto4.Models;
+
+Calculadora calculadora1 = new Calculadora();
+
+calculadora1.Somar(4,5);
+calculadora1.Subtrair(4,5);
+calculadora1.Dividir(4,5);
+calculadora1.Multiplicar(4,5);
+calculadora1.Potencia(4,5);
+calculadora1.Coseno(30);
+calculadora1.Seno(30);
+calculadora1.Tangente(30);
+calculadora1.RaizQuadrada(9);
---
+++ projeto5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.Contracts;
+
+int numero = 10;
+
+for(int contador = 0 ; contador<=numero; contador++){
+    System.Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+}
+int contador1=0;
+while(contador1 <= numero){
+    System.Console.WriteLine($"{numero} x {contador1} = {numero * contador1}");
+    contador1++;
+}
+
+int soma = 0, num = 0;
+
+do{
+    System.Console.WriteLine("digite um nuemro(0 para parar)");
+    //num = Convert.ToInt32(Console.ReadLine());
+    soma += num;
+} while(num !=0);
+
+System.Console.WriteLine($"total da somo dos numeros é {soma}");
